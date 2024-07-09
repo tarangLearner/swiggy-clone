05a8c0cc01ba559259856392421b4ddf330db75b
Tarang <tarang080391@gmail.com>
2024-07-09 17:49:59 +0530

    fetch the data from API with the help of useeffect

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 2. Reconciliation Algo aka react fiber
    - virtual DOM => its representation of DOM but not actual DOM. its collection of react element and react element is collection of object.
    - Diff algo => its difference between old vs new virtual DOM and reacts render diff between these two virtual DOM
+3. ## monolith vs microservice
+4. useEffect - it executes after the body renderes on UI. it mainly follows the concept - load -> render -> API call -> re-render. it comes with callback function and that callback function executes once first cycle of render completes.
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { resList } from "../utils/mockData";
 import ResCard from "./ResCard";
 const Body = () => {
   // whenever state variable changes, react reredner with updated values.
   // useState => state variable and method which helps us to update state variable and it will rerender the list
-  const [listOfRes, setListOfRes] = useState(resList);
+  const [listOfRes, setListOfRes] = useState([]);
   const [listForSearchRes, setListForSearchRes] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRes(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  if (listOfRes.length === 0) {
+    return <h1>Add Shimmer UI Loading ...</h1>;
+  }
 
   return (
     <div className="body">
